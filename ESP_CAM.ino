8844a9b28fe88a742be44594892358c5b7d6d1e8
sitibudi <85599618+sitibudi@users.noreply.github.com>
2022-04-25 02:28:24 +0700

    esp_cam (include library, setup void)

--- ESP_CAM.ino
+++ ESP_CAM.ino
@@ -1,31 +1,34 @@
-============================LIBRARY=========================
+//============================LIBRARY=========================
 #include "esp_camera.h"
 #include <WiFi.h>
 
-  // Select camera model
-#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+//
+// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
+//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
+//            Partial images will be transmitted if image exceeds buffer size
+//
+
+// Select camera model
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
 //#define CAMERA_MODEL_ESP_EYE // Has PSRAM
 //#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
 //#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
 //#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
 //#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
-//#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
 //#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
-  #include "camera_pins.h"
-============================DONE============================
+
+#include "camera_pins.h"
 
 
-==========================INITIALIZATION=====================
+//============================DONE============================
 
 
+//===========================FUNCTION===========================
 
-================================DONE==========================
-
-
-===========================FUNCTION===========================
 void startCameraServer();
-void mulai(){
-  Serial.setDebugOutput(true);
+void espsetup(){
+   Serial.setDebugOutput(true);
   Serial.println();
 
   camera_config_t config;
@@ -105,14 +108,20 @@ void mulai(){
   Serial.println("' to connect");
 }
 
-============================DONE===============================
+//================================DONE==========================
+
+//==========================INITIALIZATION=====================
+const char* ssid = "STUDIO GAMING";
+const char* password = "sukaayamgoreng";
+
+//============================DONE===============================
+
 void setup() {
-  // put your setup code here, to run once:
-Serial.begin(115200);
-  mulai();
+  Serial.begin(115200);
+  espsetup();
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-
+  delay(10000);
 }
