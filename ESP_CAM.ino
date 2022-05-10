3bf983b0b304fec3d74f0897926925b160a31c7c
sitibudi <85599618+sitibudi@users.noreply.github.com>
2022-05-10 23:12:32 +0700

    FINAL smart door Android Studio with Firebase

--- ESP_CAM.ino
+++ ESP_CAM.ino
@@ -1,45 +1,198 @@
 //============================LIBRARY=========================
 #include "esp_camera.h"
 #include <WiFi.h>
-
-//
-// WARNING!!! PSRAM IC required for UXGA resolution and high JPEG quality
-//            Ensure ESP32 Wrover Module or other board with PSRAM is selected
-//            Partial images will be transmitted if image exceeds buffer size
-//
-
-// Select camera model
-//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
-//#define CAMERA_MODEL_ESP_EYE // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_PSRAM // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_V2_PSRAM // M5Camera version B Has PSRAM
-//#define CAMERA_MODEL_M5STACK_WIDE // Has PSRAM
-//#define CAMERA_MODEL_M5STACK_ESP32CAM // No PSRAM
-#define CAMERA_MODEL_AI_THINKER // Has PSRAM
-//#define CAMERA_MODEL_TTGO_T_JOURNAL // No PSRAM
-
-#include "camera_pins.h"
-
-
+#include <NTPClient.h>
+#include <WiFiUdp.h>
+#include "time.h"
+#include <FirebaseESP32.h>
+#include <addons/RTDBHelper.h>
 //============================DONE============================
 
 //==========================INITIALIZATION=====================
+
+// Select camera model
+//#define CAMERA_MODEL_WROVER_KIT // Has PSRAM
+#define CAMERA_MODEL_AI_THINKER // Has PSRAM
+#include "camera_pins.h"
+
 const char* ssid = "STUDIO GAMING";
 const char* password = "sukaayamgoreng";
-unsigned long pre_relay = 0;
-bool relay_stat = false;
-unsigned long time_relay = 5000;
-byte relay = 12;
-byte button = 4;
-unsigned long current = 0;
+//authenticator FIREBASE realtime database
+#define DATABASE_URL "https://smart-door-322ac-default-rtdb.asia-southeast1.firebasedatabase.app/" //<databaseName>.firebaseio.com or <databaseName>.<region>.firebasedatabase.app
+#define DATABASE_SECRET "p4uaOxKEg3uVi4vHRQcHHcRv9ECOuWcntiw988JL"
+
+/* Define the Firebase Data object */
+FirebaseData fbdo;
+//FirebaseData fbdo1;
+//FirebaseData fbdo2;
+//FirebaseData fbdo3;
+//FirebaseData fbdo4;
+//FirebaseData fbdo5;
+//FirebaseData fbdo6;
+
+/* Define the FirebaseAuth data for authentication data */
+FirebaseAuth auth;
+
+/* Define the FirebaseConfig data for config data */
+FirebaseConfig config;
+
+//=====================NTP server time==========================================
+WiFiUDP ntpUDP;
+NTPClient timeClient(ntpUDP,"pool.ntp.org", 25200);  // GMT(+7) * 3600 = gmtOffset_sec 
+const char* ntpServer = "pool.ntp.org";
+//==========================DEFINE PIN========================================
+#define relay 14
+const int limit = 13;
+#define buzzer 12
+int dly = 3000; //delay sistem alarm
+int button = 0;
+
+
+
+
+//============================JADWAL 1===========================
+int jam1 =0;
+int menit1=0;
+//============================DONE===============================
+//============================JADWAL 2===========================
+int jam2 =0;
+int menit2=0;
+//============================DONE===============================
+//============================JADWAL 3===========================
+int jam3 =0;
+int menit3=0;
 //============================DONE===============================
 
-//===========================FUNCTION===========================
+
+
+//===========================FUNCTION============================
 
 void startCameraServer();
-void espsetup(){
-   Serial.setDebugOutput(true);
+void firebaseSetup(){
+  //========================Setup FIREBASE realtime database======================
+    Serial.printf("Firebase Client v%s\n\n", FIREBASE_CLIENT_VERSION);
+
+    /* Assign the certificate file (optional) */
+    //config.cert.file = "/cert.cer";
+    //config.cert.file_storage = StorageType::FLASH;
+
+    /* Assign the database URL and database secret(required) */
+    config.database_url = DATABASE_URL;
+    config.signer.tokens.legacy_token = DATABASE_SECRET;
+
+    Firebase.reconnectWiFi(true);
+
+    /* Initialize the library with the Firebase authen and config */
+    Firebase.begin(&config, &auth);
+  Serial.setDebugOutput(true);
   Serial.println();
+  
+}
+
+
+void JADWAL1 (){
+ 
+  //=========================================JADWAL 1 ===========================================================
+  Firebase.getString(fbdo,"/waktu1/jam1",jam1);
+  jam1 = fbdo.intData();
+  
+  Firebase.getString(fbdo,"/waktu1/menit1",menit1);
+  menit1=fbdo.intData();
+//  Serial.println(jam1);
+//  Serial.println(menit1);
+
+   if(timeClient.getHours() ==jam1 and timeClient.getMinutes()==menit1 and timeClient.getSeconds()==0){
+    digitalWrite(relay,HIGH);
+    delay(dly);
+    digitalWrite(relay,LOW);
+   
+  
+
+  }
+  else{
+    if(digitalRead(limit)==HIGH){
+    digitalWrite(buzzer,HIGH);
+  }
+    else {
+    digitalWrite(buzzer,LOW);
+  }
+  
+  }
+  
+ 
+   delay(300);
+   
+}
+void JADWAL2 (){
+  //=========================================JADWAL 2 ===========================================================
+   Firebase.getString(fbdo,"/waktu2/jam2",jam2);
+  jam2 = fbdo.intData();
+  
+  Firebase.getString(fbdo,"/waktu2/menit2",menit2);
+  menit2=fbdo.intData();
+//  Serial.println(jam2);
+//  Serial.println(menit2);
+
+   if(timeClient.getHours() ==jam2 and timeClient.getMinutes()==menit2 and timeClient.getSeconds()==0){
+    digitalWrite(relay,HIGH);
+    delay(dly);
+    digitalWrite(relay,LOW);
+//    digitalWrite(buzzer,LOW);
+  }
+  else{
+    if(digitalRead(limit)==HIGH){
+    digitalWrite(buzzer,HIGH);
+  }
+    else {
+    digitalWrite(buzzer,LOW);
+  }
+
+  
+  }
+  
+}
+void JADWAL3 (){
+   //=========================================JADWAL 3 ===========================================================
+   Firebase.getString(fbdo,"/waktu3/jam3",jam3);
+  jam3 = fbdo.intData();
+  
+  Firebase.getString(fbdo,"/waktu3/menit3",menit3);
+  menit3=fbdo.intData();
+//  Serial.println(jam3);
+//  Serial.println(menit3);
+
+   if(timeClient.getHours() ==jam3 and timeClient.getMinutes()==menit3 and timeClient.getSeconds()==0){
+    digitalWrite(relay,HIGH);
+    delay(dly);
+    digitalWrite(relay,LOW);
+
+  }
+  else{
+    if(digitalRead(limit)==HIGH){
+    digitalWrite(buzzer,HIGH);
+  }
+    else {
+    digitalWrite(buzzer,LOW);
+  }
+
+  }
+  
+}
+void espsetup(){
+  
+   WiFi.begin(ssid, password);
+    Serial.print("Connecting to Wi-Fi");
+    while (WiFi.status() != WL_CONNECTED)
+    {
+        Serial.print(".");
+        delay(300);
+    }
+    Serial.println();
+    Serial.print("Connected with IP: ");
+    Serial.println(WiFi.localIP());
+    Serial.println();
+
+
 
   camera_config_t config;
   config.ledc_channel = LEDC_CHANNEL_0;
@@ -102,56 +255,96 @@ void espsetup(){
   s->set_hmirror(s, 1);
 #endif
 
-  WiFi.begin(ssid, password);
-
-  while (WiFi.status() != WL_CONNECTED) {
-    delay(500);
-    Serial.print(".");
-  }
-  Serial.println("");
-  Serial.println("WiFi connected");
+ 
 
   startCameraServer();
 
   Serial.print("Camera Ready! Use 'http://");
   Serial.print(WiFi.localIP());
   Serial.println("' to connect");
-}
 
-void trigger(){
-  
-  if (digitalRead(button)== LOW){
-//    relay_stat = !relay_stat;
-     digitalWrite(relay,HIGH);
+ 
+ //======================Preparing NTP Time configuration=====================
      
+}
+//get local GMT time function
+void printLocalTime()
+{
+ timeClient.update();
+ timeClient.getFormattedTime();
+
+ delay(1000);
+ 
+ 
+}
+
+//security system function
+void security(){
+  
+   if(digitalRead(limit)==HIGH){
+    digitalWrite(buzzer,HIGH);
   }
-  
-  else if(digitalRead(button)==HIGH){
-  current = millis();
-  if(current-pre_relay>time_relay){
-  digitalWrite(relay,LOW);
-  pre_relay = current;
-//  Serial.println(relay_stat);
-//  Serial.println(pre_relay);
-  
+  else {
+    digitalWrite(buzzer,LOW);
   }
 }
 
-}
+
 //================================DONE==========================
 
+void runfirebase(){
+//  Read realtime database pintu
+//  int n =0;
+  int button_old;
+  
+  if(Firebase.getString(fbdo,"/relay/pintu",button)){
+  button = fbdo.intData();
+  if(button == 1){
+    digitalWrite(relay,HIGH);
+    delay(dly);
+    digitalWrite(relay,LOW);
+    button =0;
+    Firebase.setInt(fbdo,"/relay/pintu",button);
+    
+  }
+  else {
+    if(digitalRead(limit)==HIGH){
+    digitalWrite(buzzer,HIGH);
+  }
+    else {
+    digitalWrite(buzzer,LOW);
+  }
+  }
+  
+  }
+  delay(300);
+  
 
+  
+  
+}
 
 void setup() {
   Serial.begin(115200);
+  
+  
+  pinMode(relay,OUTPUT);
+  pinMode(limit,INPUT_PULLUP);
+  pinMode(buzzer,OUTPUT);
   espsetup();
-   pinMode(relay,OUTPUT);
-  pinMode(button,INPUT_PULLUP);
+  firebaseSetup();
+  timeClient.begin();
   
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
-  trigger();
-  delay(10000);
+
+  printLocalTime();
+  runfirebase();
+  JADWAL1();
+  JADWAL2();
+  JADWAL3();
+  
 }
+
